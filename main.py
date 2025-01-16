5206f4f2d7f341f68eeef36e345a09b3d1b7676e
Nand Kishor Vyas <86528339+nand2509@users.noreply.github.com>
2025-01-17 01:53:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyBH9pjDICFfC4RqNF2luI86P1LVlLyCF78")
+genai.configure(api_key=" ")
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Who is Maharaja Dr. Vibhuti Narain Singh?")
 print(response.text)
